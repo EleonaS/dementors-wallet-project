a08be5ac1d6f7309257433a73e33ea3345586eee
Elena Sysoyeva <evsysoyeva@gmail.com>
2022-04-03 17:37:59 +0300

    test

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './LoginPage.module.css';
+import Container from 'components/Container';
 // import LoginForm from "../../components/LoginForm";
 
 const LoginPage = () => {
   return (
-    <div className={s.container}>
+    <Container>
       <div className={s.wrapper}>
         <p className={s.title}>Finance App</p>
         {/* <LoginForm/> */}
       </div>
-    </div>
+    </Container>
   );
 };
 
--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './RegistrationPage.module.css';
+import Container from 'components/Container';
 // import RegistrationForm from "../../components/RegistrationForm";
 
 const RegistrationPage = () => {
   return (
-    <div className={s.container}>
+    <Container>
       <div className={s.wrapper}>
         <p className={s.title}>Finance App</p>
         {/* <RegistrationForm/> */}
       </div>
-    </div>
+    </Container>
   );
 };
 
